5d55fa80611fb4340205980128d73de7ac6a6c2e
AustinDodd <53629376+AustinDodd@users.noreply.github.com>
2019-09-09 20:45:17 -0500

    Add files via upload

---
+++ image-blur-1.rb
@@ -0,0 +1,20 @@
+class Image
+  def initialize(pixle)
+    @pixle = pixle
+  end
+
+  def output_image
+    @pixle.each do |row|
+      puts row.join
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
